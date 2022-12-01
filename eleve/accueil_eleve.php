49602b10cbdcd6fefe53bddeb7dea4a011e691f5
Claire CREMAS <claire.cremas@etu.emse.fr>
2022-12-01 11:37:01 +0100

    creation accueil prof

--- eleve/accueil_eleve.php
+++ eleve/accueil_eleve.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="accueil.css"> <!--css-->
+    <link rel="stylesheet" href="accueil_eleve.css"> <!--css-->
 </head>
 
 <?php session_start();
