6d8d5cf6a84799cf2e78307cfb9d29a487d81e50
Claire CREMAS <claire.cremas@etu.emse.fr>
2023-01-01 00:06:04 +0100

    fichier lecture Excel

---
+++ rentragenotes.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+while(!empty(notesatraiter)){
+    $fichier_note = fopen("", "rb");
+
+    if(!$fichier_note){
+        exit("Ouverture fichier impossible");
+    }
+
+    for
+    $nom = $sheet->getCellByColumnAndRow(0,i) ;
+    $prenom = $sheet->getCellByColumnAndRow(1,i) ;
+    $note = $sheet->getCellByColumnAndRow(2,i) ;
+
+
+    if(!fclose($fichier_note)){
+        exit("Fermeture fichier impossible");
+    }
+
+
+
+}
+
+
+
+
+
+
+
+?>
