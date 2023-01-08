aa187dd582ce0660e08a39cfa16815aa88a84ec1
Claire CREMAS <claire.cremas@etu.emse.fr>
2023-01-08 11:08:59 +0100

    ajoutnote.php

--- ajoutnote.php
+++ ajoutnote.php
@@ -18,85 +18,75 @@
             echo "Seuls les fichiers au format .xlsx sont autorisés." ;
         }
     }
-?>
 
-
-
-<?php 
 
 include('../vendor/autoload.php'); #appel à phpSpreadsheet
-include('../include/database.php'); #connexion à la base de donnée
-global $db; #permet d'avoir la base de donnée sous le nom db
+#include('../include/database.php'); #connexion à la base de donnée
+#global $db; #permet d'avoir la base de donnée sous le nom db
 
-use PhpOfficePhpSpreadsheetSpreadsheet;
-use PhpOfficePhpSpreadsheetReaderXlsx;
-
-
-function rentrernote($notesatraiter, $notestraitees, $id_up){
-
-    # pointeur sur le dossier des notes à rentrer
-    $rep = @opendir("../" . $notesatraiter . "/");
-    if(!$rep){
-        echo 'Erreur fichier notesatraiter';
-    }
-
-    if (isset($_POST['submit'])){
-        $nom_eval = $_POST['eval'];
-        $coef = $_POST['coef'];
-        db -> query("INSERT INTO eval (nom, id_up, Coefficient, TYPE) VALUES ('$nom_eval', $id_up, $coef, 'E')");
-    }
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+#use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
 
-    while(!empty("../" . $notesatraiter . "/")){
-        $file = readdir($rep); # on prend le nom d'un fichier dans le dossier
-        if(!$file){
-            echo "Pas de fichier dans notesatraiter" ;
-        }
+# pointeur sur le dossier des notes à rentrer
+$rep = @opendir('../notesatraiter');
+if(!$rep){
+    echo 'Erreur fichier notesatraiter';
+}
 
-        $reader = new PhpOfficePhpSpreadsheetReaderXlsx;
-        $fichier_note = $reader->load($file);
+if (isset($_POST['submit'])){
+    $nom_eval = $_POST['eval'];
+    $coef = $_POST['coef'];
+    db -> query("INSERT INTO eval (nom, id_up, Coefficient, TYPE) VALUES ('$nom_eval', $up_note, $coef, 'E')");
+}
 
-        #$fichier_note = \PhpOfficePhpSpreadsheetIOFactory::load($file);
+
+while(false !== ($file = readdir($rep))){  
+    if($file!="." && $file!=".."){
+        
+        $reader = new Xlsx();
+        $fichier_note = $reader->load("../notesatraiter/$file");
+        
+        #$fichier_note = PhpOffice\PhpSpreadsheet\IOFactory::load($file);
         if(!$fichier_note){
             echo "Ouverture notesatraiter impossible" ;
         }
-
+        
         $sheet = $fichier_note->getActiveSheet() ;
         $nb_eleves = $sheet->getCell('D1')->getCalculatedValue();
-
-
+        
+        
         for($i = 2; $i <= $nb_eleves; $i++){
-
+            
             $compteur = 0;
-
+            
             $nom = $sheet->getCellByColumnAndRow(1,$i) ;
             $prenom = $sheet->getCellByColumnAndRow(2,$i) ;
             $note = $sheet->getCellByColumnAndRow(3,$i) ;
-            $eleve -> echo(trouveideleve($nom, $prenom));
-            $db -> query("INSERT INTO note (id_user, note) VALUES ($eleve, $note)");
-
+            $id_eleve = trouveideleve($nom, $prenom);
+            $db -> query("INSERT INTO note (id_eval, id_user, note) VALUES (#creer une requete pour avoir l'id de l'eval cree $id_eleve, $note)");
+            
             $compteur++;
-
+            
         }
-
+        
         #déplacer fichier dans notestraitees
         if($compteur==$nb_eleves){
-
-            $currentLocation = "../" . $notesatraiter . "/" . $file;
-            $newLocation = "../" . $notestraitees . "/" . $file;
+            
+            $currentLocation = '../notesatraiter/'. $file;
+            $newLocation = '../notestraitees/'. $file;
             $moved = rename($currentLocation, $newLocation);
             
             if($moved){
                 echo 'Notes rentrées';
             }
-
+            
         }
         
     }
-
-    closedir($rep);
 }
-
+closedir($rep);
 ?>
 
 
