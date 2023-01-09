0332253f72cf7a4d5ac3d1ab44e672fea189c6b0
Claire CREMAS <claire.cremas@etu.emse.fr>
2023-01-09 21:21:09 +0100

    supp fichiers css superflus

--- eleve/Simulation/simulation_GP.php
+++ eleve/Simulation/simulation_GP.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="simulation_GP1.css">
+    <link rel="stylesheet" href="simulation_GP.css">
     <?php echo("<title>Simulation GP</title>"); ?>
     <style>
         h1{
